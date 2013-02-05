d748da178c0870f02ca20d8674a6249593554bd5
Tony <anthony.ratagobal@gmail.com>
2013-02-05 14:40:52 +0100

    Un clic sur un chapitre dans la table des matières affiche le chapitre en entier.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 window.onload = function() {
- 
-  
+
   var livreChoisi = document.getElementById('livre');
   livreChoisi.onchange = function() {
     ouvrirFichier(livreChoisi.files[0], function(data) {
@@ -12,12 +11,19 @@ window.onload = function() {
 	  {
 		  var parser = new DOMParser();
 		  var doc = parser.parseFromString(container, "application/xml");
+
 		  var rootfile = doc.getElementsByTagNameNS("*", "rootfile")[0];
+		
 		  var opffile = rootfile.getAttribute("full-path");
+
 		  var opfdata = zip.file(opffile);
+		 
 		  var opf = parser.parseFromString(opfdata.data, "text/xml");
+
 		  var manifest = opf.getElementsByTagNameNS("*", "manifest")[0];
+
 		  var spine = opf.getElementsByTagNameNS("*", "spine")[0];
+
 
 		  var toc = spine.getAttribute("toc");
 		  
@@ -33,19 +39,35 @@ window.onload = function() {
 			if (id == idRef) {
 			  var nomfichierHTML = items[i].getAttribute("href");
 			} else if(id == toc) {
+				
 			  var nomfichierNcx = items[i].getAttribute("href");
 			}
 		  }
+		  var OPS='';
 		  
-		  var fichierNcx = zip.file(nomfichierNcx);
+		  var verifExistenceOPS=zip.folder("OPS").file("toc.ncx");//Si le fichier existe dans OPS alors OPS existe
+
+		  if(verifExistenceOPS)
+		  {
+			OPS='OPS/';
+		  }
+		  else
+		  {
+			OPS='';
+		  }
+
+		  var fichierNcx = zip.file(OPS+nomfichierNcx);
+
 		  var decoded = decodeUTF8(fichierNcx.data);
 		  var parserNcx = parser.parseFromString(decoded, "text/xml");
 		  var navMap = parserNcx.getElementsByTagNameNS("*", "navMap")[0];
 		  var navPoints = navMap.getElementsByTagNameNS("*", "navPoint");
 		  var listeDePoints = [];
+		  var pageFiles=[];
+		  
 		   if (navPoints.length > 0) {
 		   var compteurPoints = 0;
-		   
+
 		   for (var i = 0; i < navPoints.length; i++) {
 				  
 					var label = navPoints[i].getElementsByTagNameNS("*", "navLabel")[0]
@@ -61,7 +83,18 @@ window.onload = function() {
 					listeDePoints[compteurPoints] = point;
 					compteurPoints++;
 					
-					console.log(label);
+					if(OPS=='')
+					{
+						var pageFile = zip.file(content);
+						
+					}
+					else
+					{
+						var pageFile = zip.file(OPS+content.substring(0,15));
+					}
+					decodedPage=decodeUTF8(pageFile.data);
+					pageFiles.push(decodedPage);
+
 				  
 			}
 			
@@ -72,7 +105,10 @@ window.onload = function() {
 		  {
 		  var chapitre = document.createElement('li');
 		  var nomChapitre = document.createTextNode(listeDePoints[i].label);
-		  
+		  var divChapitre ='' ;
+		  divChapitre = createNewDiv(pageFiles[i]);
+
+		  chapitre.onclick=afficherChapitre(divChapitre);
 		  chapitre.appendChild(nomChapitre);
 		  listChapitre.appendChild(chapitre);
 		  }
@@ -83,7 +119,38 @@ window.onload = function() {
   };  
 };
 
-  function decodeUTF8(str) {
+  function createNewDiv(chapitre) {
+  
+	var divChapitre=document.createElement("div");
+	divChapitre.id = 'chapitre';
+	divChapitre.className = 'chapitre';
+	divChapitre.innerHTML=chapitre;
+
+    return divChapitre;
+  }
+
+function afficherChapitre(page)
+{
+		return function()
+	{
+			var divContainerChapitre = document.getElementById("containerChapitre"); 
+
+			if(document.getElementById("chapitre"))
+			{
+				var element=document.getElementById("chapitre");
+				element.parentNode.removeChild(element);
+				document.getElementById("containerChapitre").appendChild(page);
+			}
+			else
+			{
+				document.getElementById("containerChapitre").appendChild(page);
+			}
+
+	}
+}
+
+
+function decodeUTF8(str) {
     return decodeURIComponent(escape(str));
   }
   
@@ -106,5 +173,6 @@ function getContainer(archive) {
     }
   
 }
+
 
 
