fa1ef5c1307f1f993e75b54b6e134de085842fc3
Philippe Benoit <contact@philippe-benoit.fr>
2014-02-08 14:19:24 +0100

    [Cleanup] Cleaning indexDB.js

--- js/cls/indexedDB.js
+++ js/cls/indexedDB.js
@@ -6,151 +6,141 @@ window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.ms
  
  //Create database
 function createDatabase(event) {
-    var db = event.target.transaction.db;
-    var bookStore = db.createObjectStore("books",{keyPath: "bookTitle"});
+  var db = event.target.transaction.db;
+  var bookStore = db.createObjectStore("books", {keyPath: "bookTitle"});
 }
 
 /**
 * callback when there is an erro to open DB
 */
 function errorOpen(event) {
-    window.alert("Open error !");
+  window.alert("Open error !");
 }
 
 /**
 * .
 * Add or replace a book
 */
-function savesBook(file,bookTitle,bookAuthor) {
+function savesBook(file, bookTitle, bookAuthor) {
 
- //Create the object
+	//Create the object
 	var book = {
 		bookFile: file,
 		bookTitle: bookTitle,
 		bookAuthor: bookAuthor
 	}
-	var alreadyStored=false;
-    // open DB
-    var request = indexedDB.open("booksLibrary2", 1);
-    request.onerror = errorOpen;
-    request.onupgradeneeded = createDatabase;
+	
+	var alreadyStored = false;
+  
+	// open DB
+  var request = indexedDB.open("booksLibrary2", 1);
+  request.onerror = errorOpen;
+  request.onupgradeneeded = createDatabase;
 
-    request.onsuccess = function(event) {
+  request.onsuccess = function(event) {
        
-        var db = event.target.result;
-        // Open transaction
-        var transaction = db.transaction(["books"], "readwrite");
-        transaction.oncomplete = function(event) {
-        };
+    var db = event.target.result;
+    // Open transaction
+    var transaction = db.transaction(["books"], "readwrite");
+    transaction.oncomplete = function(event) {};
 
-        transaction.onerror = function(event) {
-           window.alert('Transaction error ');
-        };
+    transaction.onerror = function(event) {
+       window.alert('Transaction error ');
+    };
 
-        // Get the store
-        var bookStore = transaction.objectStore("books");
+    // Get the store
+    var bookStore = transaction.objectStore("books");
 
 		// Verify if the book is already stored
 		bookStore.openCursor().onsuccess = function (event) {
-
 			var cursor = event.target.result;
 			if (cursor) {
-				//console.log(cursor);
 				var getBook = bookStore.get(cursor.key);
-
 				getBook.onsuccess = function (e) {
-					if(getBook.result.bookFile.name == file.name)
-					{
+					if (getBook.result.bookFile.name === file.name) {
 						alreadyStored = true;
 					}
 					cursor.continue();
 				}
 			}
-
 		}
 		
-		var bookList = document.getElementById('bookList');
+		var book$List = document.getElementById('bookList');
 		var id = bookList.childNodes.length;
 
-		if(alreadyStored != true)
-		{
+		if (alreadyStored !== true) {
 			var req = bookStore.put(book);
 				req.onsuccess = function(event) {
-				//console.log('stockage OK!!!!');
 			}
 			req.onerror = function(event) {
 				window.alert('erreur ajout');
 			}
-		}
-		else
-		{
+		}	else {
 			alert('This book is already in your library');
 		}
-    }
+  }
 }
 
-
-
-//Get book by ID
+/**
+ *Get book by ID
+ */
 function getBook(id) {
 
-// on ouvre la base, et on déclare les listeners
-    var request = indexedDB.open("booksLibrary2", 1);
+	// on ouvre la base, et on dï¿½clare les listeners
+  var request = indexedDB.open("booksLibrary2", 1);
 	var resultat = "";
-    request.onerror = errorOpen;
-    request.onupgradeneeded = createDatabase;
+	request.onerror = errorOpen;
+	request.onupgradeneeded = createDatabase;
 
-    request.onsuccess = function(event) {
-        // ici la base a été ouverte avec succés, il faut ajouter l'enregistrement
+	request.onsuccess = function(event) {
+		// ici la base a ï¿½tï¿½ ouverte avec succï¿½s, il faut ajouter l'enregistrement
 
-        // on récupère l'objet database
-        var db = event.target.result;
+		// on rï¿½cupï¿½re l'objet database
+		var db = event.target.result;
 
-        // on ouvre une transaction qui permettra d'effectuer
-        // les opérations sur la base
-        var transaction = db.transaction(["books"], "readwrite");
-        transaction.oncomplete = function(event) {
-            window.alert("Transaction terminée, sauvegarde effectuée");
-        };
+		// on ouvre une transaction qui permettra d'effectuer
+		// les opï¿½rations sur la base
+		var transaction = db.transaction(["books"], "readwrite");
+		transaction.oncomplete = function(event) {
+				window.alert("Transaction terminÃ©e, sauvegarde effectuÃ©e");
+		};
 
-        transaction.onerror = function(event) {
-           window.alert('erreur de transaction ');
-        };
+		transaction.onerror = function(event) {
+			 window.alert('erreur de transaction ');
+		};
 
-        // on récupère l'object store dans lequel on veut stocker l'objet
-        var sauvegardeStore = transaction.objectStore("books");
+		// on rï¿½cupï¿½re l'object store dans lequel on veut stocker l'objet
+		var sauvegardeStore = transaction.objectStore("books");
 
-        // on créé l'ordre d'ajouter un enregistrement
-        // sera effectivement executé lors de la fermeture de la transaction
-        var req = sauvegardeStore.get(id);
+		// on crï¿½ï¿½ l'ordre d'ajouter un enregistrement
+		// sera effectivement executï¿½ lors de la fermeture de la transaction
+		var req = sauvegardeStore.get(id);
 
-        req.onsuccess = function(event) {
-			if(req.result)
-			{
+    req.onsuccess = function(event) {
+			if (req.result) {
 				resultat = req.result;
 				book = req.result;
-				//console.log(book);
-			}
-			else 
-			{
+			} else {
 				alert("Bug");
 			}
-        }
-
-        req.onerror = function(event) {
-            window.alert('erreur ajout');
-        }
     }
+
+		req.onerror = function(event) {
+				window.alert('erreur ajout');
+		}
+	}
 }
 
-// Get all books and store them into global variable booksArray
+/**
+ * Get all books and store them into global variable booksArray
+ */
 function getAllBooks(){
 
-	// on ouvre la base, et on déclare les listeners
+	// on ouvre la base, et on dï¿½clare les listeners
 	var request = indexedDB.open("booksLibrary2", 1);
 	var resultat = "";
-		request.onerror = errorOpen;
-		request.onupgradeneeded = createDatabase;
+	request.onerror = errorOpen;
+	request.onupgradeneeded = createDatabase;
 
 	request.onsuccess = function(event) {
 		var db = event.target.result;
@@ -162,16 +152,13 @@ function getAllBooks(){
 			window.alert('erreur de transaction lecture ');
 		};
 
-		// on récupère l'object store que l'on veut lire
+		// on rï¿½cupï¿½re l'object store que l'on veut lire
 		var bookStore = transaction.objectStore("books");
 		
 		bookStore.openCursor().onsuccess = function (event) {
-
 			var cursor = event.target.result;
 			if (cursor) {
-				
 				var getBook = bookStore.get(cursor.key);
-
 				getBook.onsuccess = function (e) {
 					booksArray.push(getBook.result); // un niveau
 					cursor.continue();
@@ -183,10 +170,10 @@ function getAllBooks(){
 }
 
 /**
-Delete all books in the DB
+ * Delete all books in the DB
  */
 function deleteAllBooks() {
-    // on ouvre la base, et on déclare les listeners
+    // on ouvre la base, et on dï¿½clare les listeners
     var request = indexedDB.open("booksLibrary2", 1);
     request.onerror = errorOpen;
     request.onupgradeneeded = createDatabase;
@@ -211,77 +198,70 @@ function deleteAllBooks() {
     }
 }
     
-	
-//Delete a book by ID
+/**	
+ * Delete a book by ID
+ */
 function deleteBookConfirm(bookTitle) {
-
-	return function()
-	{
-		if(document.getElementById('confirmDeleteBook').style.display = "none")
-		{
+	return function() {
+		if (document.getElementById('confirmDeleteBook').style.display === "none") {
 			document.getElementById('confirmDeleteBook').style.display = "block";
-			document.getElementById('btnDeleteBook').onclick=deleteBook(bookTitle);
+			document.getElementById('btnDeleteBook').onclick = deleteBook(bookTitle);
 		}
 	}
 }
 
 //Delete a book by ID
 function deleteBook(bookTitle) {
-	
-	return function ()
-	{
-		// on ouvre la base, et on déclare les listeners
+	return function () {
+		// on ouvre la base, et on dï¿½clare les listeners
 		var request = indexedDB.open("booksLibrary2", 1);
 		var resultat = "";
 		request.onerror = errorOpen;
 		request.onupgradeneeded = createDatabase;
 
 		request.onsuccess = function(event) {
-			// ici la base a été ouverte avec succés, il faut ajouter l'enregistrement
+			// ici la base a ï¿½tï¿½ ouverte avec succï¿½s, il faut ajouter l'enregistrement
 
-			// on récupère l'objet database
+			// on rï¿½cupï¿½re l'objet database
 			var db = event.target.result;
 
 			// on ouvre une transaction qui permettra d'effectuer
-			// les opérations sur la base
+			// les opï¿½rations sur la base
 			var transaction = db.transaction(["books"], "readwrite");
-			transaction.oncomplete = function(event) {
-				
-			};
+			transaction.oncomplete = function(event) {};
 
 			transaction.onerror = function(event) {
 			   window.alert('erreur de transaction ');
 			};
 
-			// on récupère l'object store dans lequel on veut stocker l'objet
+			// on rï¿½cupï¿½re l'object store dans lequel on veut stocker l'objet
 			var bookStore = transaction.objectStore("books");
 
-			// on créé l'ordre d'ajouter un enregistrement
-			// sera effectivement executé lors de la fermeture de la transaction
+			// on crï¿½ï¿½ l'ordre d'ajouter un enregistrement
+			// sera effectivement executï¿½ lors de la fermeture de la transaction
 			var req = bookStore.delete(bookTitle);
 
 			req.onsuccess = function(event) {
 				displayAllBooks();
 				cancelConfirmDeleteBook();
 			}
+			
 			req.onerror = function(event) {
 				window.alert('erreur ajout');
 			}
 
 		}
 	}
-
 }
 
-
 // Get all books and store them into global variable booksArray
-function displayAllBooks(){
+function displayAllBooks() {
 
-	// on ouvre la base, et on déclare les listeners
+	// on ouvre la base, et on dï¿½clare les listeners
 	var request = indexedDB.open("booksLibrary2", 1);
 	var resultat = "";
-		request.onerror = errorOpen;
-		request.onupgradeneeded = createDatabase;
+	request.onerror = errorOpen;
+	request.onupgradeneeded = createDatabase;
 
 	request.onsuccess = function(event) {
 		var db = event.target.result;
@@ -293,10 +273,10 @@ function displayAllBooks(){
 			window.alert('erreur de transaction lecture ');
 		};
 
-		// on récupère l'object store que l'on veut lire
+		// on rï¿½cupï¿½re l'object store que l'on veut lire
 		var bookStore = transaction.objectStore("books");
 		var booklist = document.getElementById('bookList');
-		bookList.innerHTML ="";
+		bookList.innerHTML = "";
 
 		var i = 0;
 		bookStore.openCursor().onsuccess = function (event) {
@@ -307,33 +287,26 @@ function displayAllBooks(){
 				var getBook = bookStore.get(cursor.key);
 
 				getBook.onsuccess = function (e) {
-					//console.log(getBook.result);
 					var li = document.createElement("li");
 					var chapter = document.createElement('li');
 					var dl = document.createElement('dl');
 					var dt = document.createElement('dt');
-					var chapterName = document.createTextNode(decodeUTF8(i+". "+getBook.result.bookTitle)+" - "+decodeUTF8(getBook.result.bookAuthor));
+					var chapterName = document.createTextNode(decodeUTF8(i + ". " + getBook.result.bookTitle) + " - " + decodeUTF8(getBook.result.bookAuthor));
 					var img = document.createElement('img');
 					img.src = 'images/icons/delete.png';
 					img.onclick = deleteBookConfirm(getBook.result.bookTitle);	
-					
-					// currentChapterTitle=getBook.result.bookTitle;
-					
+										
 					dl.appendChild(dt);
-					li.appendChild(dl);
-					
+					li.appendChild(dl);					
 					li.appendChild(img);
-					//li.onclick=openBook(booksArray[i].bookName);
 					
 					dt.onclick = openBook(getBook.result.bookFile);
-					//dt.onclick=getInfo(getBook.result.bookTitle);
 					dt.appendChild(chapterName);
 					booklist.appendChild(li);
 					cursor.continue();
 				}
 			}
-		}
-		
+		}	
 	}
 }
 
@@ -347,8 +320,7 @@ function deleteDB(indexedDBName) {
 		dbreq.onerror = function (event) {
 			console.log("indexedDB.delete Error: " + event.message);
 		}
-	}
-   catch (e) {
+	} catch (e) {
 		output_trace("Error: " + e.message);
 	}
 }
